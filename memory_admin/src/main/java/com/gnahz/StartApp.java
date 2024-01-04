9534b6143e47b59a18dcc4b9219097a7dac2a197
zhangweijei <zhangweijie@qq.com>
2024-01-04 13:54:15 +0800

    测试

---
+++ memory_admin/src/main/java/com/gnahz/StartApp.java
@@ -0,0 +1,16 @@
+package com.gnahz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 张伟洁
+ * Date:2024-01-04-13:52
+ * @create 忆项目(小白)
+ */
+@SpringBootApplication
+public class StartApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+}
